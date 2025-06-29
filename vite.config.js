ce86a830acd0fc366477b2c2dbaa9b026c83b582
RabelaniRamaru82 <rabelani.ramaru822305@gmail.com>
2025-06-29 17:34:31 +0200

    Updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
-    envDir: '../../',
+
     plugins: [react()],
     resolve: {
         alias: {
